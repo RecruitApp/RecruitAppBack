3161df9effd2b4155f447cea8545c6a1e2f19bd5
muhammad-tounsi <muhammad.tounsi@hotmail.fr>
2020-05-11 23:37:06 +0200

    Feat: Add models and Repository Lead, Offer and LeadStatus

---
+++ api/src/Entity/Lead.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\LeadRepository")
+ */
+class Lead
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sexe;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $age;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $motivation;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $salaryClaim;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getMotivation(): ?string
+    {
+        return $this->motivation;
+    }
+
+    public function setMotivation(?string $motivation): self
+    {
+        $this->motivation = $motivation;
+
+        return $this;
+    }
+
+    public function getSalaryClaim(): ?float
+    {
+        return $this->salaryClaim;
+    }
+
+    public function setSalaryClaim(?float $salaryClaim): self
+    {
+        $this->salaryClaim = $salaryClaim;
+
+        return $this;
+    }
+}
---
+++ api/src/Entity/Offer.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\OfferRepository")
+ */
+class Offer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $companyDescription;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $offerDescription;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $startDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $typeOfContract;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $workplace;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCompanyDescription(): ?string
+    {
+        return $this->companyDescription;
+    }
+
+    public function setCompanyDescription(string $companyDescription): self
+    {
+        $this->companyDescription = $companyDescription;
+
+        return $this;
+    }
+
+    public function getOfferDescription(): ?string
+    {
+        return $this->offerDescription;
+    }
+
+    public function setOfferDescription(string $offerDescription): self
+    {
+        $this->offerDescription = $offerDescription;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getTypeOfContract(): ?string
+    {
+        return $this->typeOfContract;
+    }
+
+    public function setTypeOfContract(string $typeOfContract): self
+    {
+        $this->typeOfContract = $typeOfContract;
+
+        return $this;
+    }
+
+    public function getWorkplace(): ?string
+    {
+        return $this->workplace;
+    }
+
+    public function setWorkplace(string $workplace): self
+    {
+        $this->workplace = $workplace;
+
+        return $this;
+    }
+}
